341fb01ad5cdc912b5fac135947d111266a38ae3
Jakub Okla <287337@student.pwr.edu.pl>
2026-01-26 22:57:23 +0100

    dodałem naiwne generatory tabel w setup_databese.sql oraz plik nuke.sql ktory usuwa wyszystkie tabele (chyba dziala)

---
+++ kody/setup_database.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Pracownicy (
+    id_pracownika INT,
+    imie VARCHAR(50),
+    nazwisko VARCHAR(50),
+    numer_telefonu VARCHAR(12),
+    kod_pocztowy VARCHAR(10),
+    miasto VARCHAR(50),
+    ulica  VARCHAR(100),
+    wynagrodzenie INT,
+)
