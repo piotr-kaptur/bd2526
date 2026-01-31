ad73f5b1300781c5f41591ec66256ee40be6be9f
Jakub Okla <287337@student.pwr.edu.pl>
2026-01-31 17:41:33 +0100

    dodalem nuke sql, ktory dziala dla moich tabel ( setup_databese.sql)

---
+++ kody/nuke.sql
@@ -0,0 +1,15 @@
+/* tutaj macie do usuwania wszystkich tabel jakby ktos potrzebowal powinno dzialac */
+
+DROP TABLE IF EXISTS
+wyniki_kontroli,
+kontrole,
+substancje_zakazane,
+wyniki_zawodow,
+chomiki,
+konkurencje,
+zawody,
+finansowanie,
+sponsorzy_umowy,
+sponsorzy,
+pracownicy
+CASCADE;
